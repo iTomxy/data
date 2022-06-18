74a1082953fde5528ffb718f6fde441f1f42ca03
iTom <44149820+iTomxy@users.noreply.github.com>
2022-06-18 19:49:48 +0800

    use relative path for easy linking

--- nuswide/make.image.link.py
+++ nuswide/make.image.link.py
@@ -6,6 +6,12 @@ import platform
 """
 soft-link all images into a single folder,
 with the name modified to their (0-base) sample ID.
+
+Hint:
+Use RELATIVE source path in linux,
+then you can simply soft-link that `images/` in any project
+instead of creating a new project-specific `images/`.
+But this trick does NOT work in Windows.
 """
 
 
@@ -15,23 +21,28 @@ cvt_sep = lambda p: p.replace('\\/'.replace(os.sep, ''), os.sep)
 
 P = cvt_sep("G:/dataset/NUSWIDE")
 IMAGE_LIST = osp.join(P, "ImageList", "Imagelist.txt")
-IMAGE_SRC = osp.join(P, "Flickr")
-IMAGE_DEST = osp.join(os.getcwd(), "images")  # path you place `images/` in
+IMAGE_SRC_ABS = osp.join(P, "Flickr")  # absolute source path
+IMAGE_SRC_REL = osp.join("..", "Flickr")  # relative source path
+IMAGE_DEST = osp.join(P, "images")  # path you place `images/` in
 if not osp.exists(IMAGE_DEST):
     os.makedirs(IMAGE_DEST)
 
 # soft-linking command
 if "Windows" == platform.system():
     cmd = "mklink {1} {0} > nul"
+    img_src_path_pre = IMAGE_SRC_ABS
 else:
     assert "Linux" == platform.system()
     cmd = "ln -s {0} {1}"
+    img_src_path_pre = IMAGE_SRC_REL
 
-print("soft-linking:", IMAGE_SRC, "->", IMAGE_DEST)
+print("soft-linking:", IMAGE_SRC_ABS, "->", IMAGE_DEST)
 with open(IMAGE_LIST, "r") as f:
     for sid, line in enumerate(f):
         line = cvt_sep(line).strip()
-        img_p = osp.join(IMAGE_SRC, line)
+        # img_p = osp.join(IMAGE_SRC_ABS, line)  # use this in Windows
+        # img_p = osp.join(IMAGE_SRC_REL, line)  # use this in linux
+        img_p = osp.join(img_src_path_pre, line)
         new_img_p = osp.join(IMAGE_DEST, "{}.jpg".format(sid))
         os.system(cmd.format(img_p, new_img_p))
         if sid % 1000 == 0:
