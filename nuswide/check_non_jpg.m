9fe9c090b1cf7732179172f63581cee1430bbfb4
iTom <44149820+iTomxy@users.noreply.github.com>
2022-06-19 22:17:03 +0800

    deal with non-jpeg files

---
+++ nuswide/check_non_jpg.m
@@ -0,0 +1,54 @@
+%% Todo
+% There are some image files are actually NOT jpeg files
+% which will raise error warning in `vl_imreadjpeg`.
+% Find out those fake jpeg files,
+% and re-save them with python cv2/PIL.Image.
+%% References
+% - https://ch.mathworks.com/matlabcentral/answers/364719-detect-warning-and-take-action
+function check_non_jpg(varargin)
+    args = inputParser;
+    addOptional(args, 'image_path', "/home/dataset/nuswide/images");
+    addOptional(args, 'matconvnet_path', "/home/tom/Download/matconvnet-1.0-beta25");
+    addOptional(args, 'o', "fake-jpg.txt");
+    parse(args, varargin{:});
+
+    this_path = cd(args.Results.matconvnet_path);
+    setup;
+    cd(this_path);
+    warning(''); % clear old warning message
+
+    file_list = dir(fullfile(args.Results.image_path, "*.jpg"));
+    fprintf("#images: %d\n", length(file_list));
+    n_fake = 0;  % count fake jpeg files
+    log_file = fopen(args.Results.o, "w");
+    for i = 1 : length(file_list)
+        img_file = file_list(i).name;
+        % sid = split(img_file, ".jpg");
+        % sid = str2num(sid{1});
+
+        % tentative loading
+        img = vl_imreadjpeg({char(fullfile(args.Results.image_path, img_file))});
+        % if that's a fake jpeg file, a warning will be raised & catched below
+        [warnMsg, warnId] = lastwarn;
+        if ~isempty(warnMsg)
+            % format: <image ID>.jpg
+            fprintf("%s\n", img_file);
+            fprintf(log_file, "%s\n", img_file);
+            warning(''); % clear old warning message
+            n_fake += 1;
+        end
+
+        if mod(i, 1000) == 0
+            fprintf("%d\n", i);
+        end
+        % break
+    end
+    if 0 == n_fake
+        fprintf(log_file, "NO FAKE JPEG FOUND\n");
+    end
+    fclose(log_file);
+    % if 0 == n_fake
+    %     fprintf("NO fake jpeg, deleting the output file: %s\n", args.Results.o);
+    %     system(sprintf("rm %s", args.Results.o));
+    % end
+end % test_non_jpg
---
+++ nuswide/replace-fake-jpg.py
@@ -0,0 +1,36 @@
+import os
+import os.path as osp
+import cv2
+from PIL import Image
+import numpy as np
+
+
+"""replace the fake jpeg detected by `check_non_jpg.m`
+Usage
+1. Before: run `check_non_jpg.m` to find out those fake jpeg files first.
+2. After: re-run `check_non_jpg.m` for validation.
+"""
+
+
+P = "/home/dataset/nuswide"
+IMAGE_PATH = osp.join(P, "images")
+FAKE_LIST_F = "fake-jpg.txt"
+
+with open(FAKE_LIST_F, "r") as f:
+    for line in f:
+        img_p = osp.join(IMAGE_PATH, line.strip())
+        img = cv2.imread(img_p)#[:, :, ::-1]
+        if img is None:
+            with Image.open(img_p) as _img_f:
+                img = np.asarray(_img_f)
+            if 2 == img.ndim:
+                img = np.repeat(img[:, :, np.newaxis], 3, axis=2)
+        else:
+            img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+
+        # remove old link
+        os.remove(img_p)
+        # replace
+        img_bgr = cv2.cvtColor(img, cv2.COLOR_RGB2BGR)
+        cv2.imwrite(img_p, img_bgr)
+print("DONE")
