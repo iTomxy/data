62214e542034606ad651e7717a31dc8779f14e1b
iTom <44149820+iTomxy@users.noreply.github.com>
2022-06-20 20:50:12 +0800

    make f25k class emb in 104 & record statistics

--- flickr25k/make.class-emb.py
+++ flickr25k/make.class-emb.py
@@ -7,18 +7,18 @@ import scipy.io as sio
 from gensim.models import KeyedVectors, Word2Vec
 
 
-"""extract class embeddings
-using GloVe features
+"""extract class embeddings (using GloVe)
+NOTE: different from the Google News version.
 """
 
 
-glove_p = "/usr/local/dataset/GloVe"
+glove_p = "/home/dataset/GloVe"
 which = "840B.300d"  # "6B.300d"  # "42B.300d"
 w2v_f = osp.join(glove_p, "w2v.glove.{}.txt".format(which))
 w2v = KeyedVectors.load_word2vec_format(w2v_f, binary=False)
 # usage: w2v[str] -> numpy array
 
-data_p = "/usr/local/dataset/flickr"
+data_p = "/home/dataset/flickr"
 # label_p = osp.join(data_p, "mirflickr25k_annotations_v080")
 # test_ls = os.listdir(label_p)
 # test_ls = [f for f in test_ls if ("_r1" not in f) and ("README" not in f)]
@@ -45,5 +45,6 @@ with open(osp.join(data_p, "class-name.flicrk25k.txt"), "r") as f:
 
 cls_emb = np.vstack(cls_emb)
 print("class emb:", cls_emb.shape, cls_emb.min(), cls_emb.max(), cls_emb.mean())
+# class emb: (24, 300) -3.0838 5.1571 0.00011771765
 sio.savemat(osp.join(data_p, "class_emb.glove-{}.flickr25k.mat".format(
     which)), {"class_emb": cls_emb})
