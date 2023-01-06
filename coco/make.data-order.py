096708973e7b118bf1ad9f5bd86a8e8ffa7d5285
iTom <44149820+iTomxy@users.noreply.github.com>
2023-01-06 18:46:14 +0800

    add log of original splitting

--- coco/make.data-order.py
+++ coco/make.data-order.py
@@ -15,18 +15,27 @@ COCO_P = "/home/dataset/COCO"
 TRAIN_P = osp.join(COCO_P, "train2017")
 VAL_P = osp.join(COCO_P, "val2017")
 
-file_list = [f for f in os.listdir(TRAIN_P) if (".jpg" in f)]
-file_list.extend([f for f in os.listdir(VAL_P) if (".jpg" in f)])
+file_list_train = [f for f in os.listdir(TRAIN_P) if (".jpg" in f)]
+file_list_val = [f for f in os.listdir(VAL_P) if (".jpg" in f)]
+file_list = file_list_train + file_list_val
 print("#data:", len(file_list))  # 12,3287
 
 id_key = lambda x: int(x.split(".jpg")[0])
 file_list = sorted(file_list, key=id_key)  # ascending of image ID
 # print(file_list[:15])
 
-with open(osp.join(COCO_P, "id-map.COCO.txt"), "w") as f:
+with open(osp.join(COCO_P, "id-map.COCO.txt"), "w") as f, \
+        open(osp.join(COCO_P, "id-map.COCO.train.txt"), "w") as f_train, \
+        open(osp.join(COCO_P, "id-map.COCO.val.txt"), "w") as f_val:
     # format: <original id> <image file name>
     for f_name in file_list:
         _original_id = id_key(f_name)
-        f.write("{} {}\n".format(_original_id, f_name))
+        _text = "{} {}\n".format(_original_id, f_name)
+        f.write(_text)
+        if f_name in file_list_val:
+            f_val.write(_text)
+        else:
+            assert f_name in file_list_train, "* Wild file name: " + f_name
+            f_train.write(_text)
         # if i > 5: break
 print("DONE")
