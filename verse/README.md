5cd9dea9ca8ffc4cd1193a0d955ce692dd6449a1
iTom <44149820+iTomxy@users.noreply.github.com>
2023-09-14 11:36:29 +1000

    add VerSe

---
+++ verse/README.md
@@ -0,0 +1,3 @@
+# References
+
+1. (MIA 2021) VerSe: A Vertebrae Labelling and Segmentation Benchmark for Multi-detector CT Images - [paper](https://www.sciencedirect.com/science/article/pii/S1361841521002127), [github](https://github.com/anjany/verse)
---
+++ verse/download.sh
@@ -0,0 +1,3 @@
+wget -c https://s3.bonescreen.de/public/VerSe-complete/dataset-verse20training.zip
+wget -c https://s3.bonescreen.de/public/VerSe-complete/dataset-verse20validation.zip
+wget -c https://s3.bonescreen.de/public/VerSe-complete/dataset-verse20test.zip
