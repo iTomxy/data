47a4f67728dec37f774c185e2653c6b696d5dfd9
iTom <tianyou.liang@student.uts.edu.au>
2024-06-21 15:31:38 +1000

    also use npz for verse & pelvic1k

--- verse/slice.py
+++ verse/slice.py
@@ -1,45 +1,52 @@
-import os, os.path as osp
+import os, os.path as osp, glob
 import numpy as np
 import medpy.io as medio
 
 """
-Slice preprocessed .npy files by ./preprocess.py (and ./complete_label.py).
+Slice preprocessed .npy files by ./preprocess.py and ./complete_label.py.
+- (2024.6.21) Compress image, label & TS label together in an npz file.
 """
 
-def slice_nii(src_dir, dest_dir, slice_axis):
-    """
-    src_dir: str, path to (subset directories of) processed nii
-    dest_dir: str, path to save slices
-    slice_axis: int, in {0, 1, 2}, along which axis to slice
-    """
-    for subset in os.listdir(src_dir):
-        print('\t', subset)
-        subset_d = osp.join(src_dir, subset)
-        for f in os.listdir(subset_d):
-            print(f, end='\r')
-            # sub-verse012_ct.nii.gz, sub-verse012_seg-vert_msk.nii.gz, sub-verse012_label_ts.nii.gz
-            stem = f[:-7]
-            slice_p = osp.join(dest_dir, subset, stem)
-            os.makedirs(slice_p, exist_ok=True)
-            # vol = np.load(osp.join(subset_d, f))
-            vol, _ = medio.load(osp.join(subset_d, f))
-            if stem.endswith("_msk") or stem.endswith("_label_ts"): # label, use int
-                vol = vol.astype(np.uint8) # class id in [0, 28]
-            # if osp.isdir(slice_p):
-            #     if os.listdir(slice_p) == vol.shape[slice_axis]: # skip finished
-            #         continue
-            # else:
-            #     os.makedirs(slice_p)#, exist_ok=True)
-            if 0 != slice_axis:
-                vol = np.moveaxis(vol, slice_axis, 0)
-            for i in range(vol.shape[0]):
-                # if osp.isfile(osp.join(slice_p, f"{stem}_{i}.npy")): # skip finished
-                #     continue
-                print(i, end='\r')
-                s = vol[i]
-                np.save(osp.join(slice_p, f"{stem}_{i}"), s)
-
+SLICE_AXIS = 2 # RAI, slice along the IS-axis
 
 P = osp.expanduser("~/sd10t/verse")
-slice_nii(osp.join(P, "processed-verse19"), osp.join(P, "processed-verse19-h-np"), 2)
-slice_nii(osp.join(P, "processed-verse19-ts-bone-label"), osp.join(P, "processed-verse19-ts-bone-label-h-np"), 2)
+il_path = osp.join(P, "processed-verse19")
+tslab_path = osp.join(P, "processed-verse19-ts-bone-label")
+save_path = osp.join(P, "processed-verse19-slice-is")
+
+for subset in os.listdir(il_path):
+    print('\t', subset)
+    sub_il_p = osp.join(il_path, subset)
+    sub_tslab_p = osp.join(tslab_path, subset)
+    for img_f in glob.glob(osp.join(sub_il_p, "*_image.nii.gz")):
+        vid = osp.basename(img_f)[: - len("_image.nii.gz")]
+        print('\t', vid, end='\r')
+
+        lab_f = osp.join(sub_il_p, f"{vid}_label.nii.gz")
+        tslab_f = osp.join(sub_tslab_p, f"{vid}_label_ts.nii.gz")
+
+        slice_p = osp.join(save_path, subset, vid)
+        if osp.isdir(slice_p):
+            continue
+
+        tmp_slice_p = slice_p + "_tmp"
+        os.makedirs(tmp_slice_p, exist_ok=True)
+
+        img, _ = medio.load(img_f)
+        lab, _ = medio.load(lab_f)
+        tslab, _ = medio.load(tslab_f)
+        assert img.shape == lab.shape and img.shape == tslab.shape
+
+        lab = lab.astype(np.uint8) # class id in [0, 28]
+        tslab = tslab.astype(np.uint8) # class id in {0, 1}
+
+        if 0 != SLICE_AXIS:
+            img = np.moveaxis(img, SLICE_AXIS, 0)
+            lab = np.moveaxis(lab, SLICE_AXIS, 0)
+            tslab = np.moveaxis(tslab, SLICE_AXIS, 0)
+
+        for i in range(img.shape[0]):
+            np.savez_compressed(osp.join(tmp_slice_p, str(i)), image=img[i], label=lab[i], ts_label=tslab[i])
+            print(i, end='\r')
+
+        os.rename(tmp_slice_p, slice_p)
