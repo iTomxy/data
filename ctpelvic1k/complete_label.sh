58a37a87d13b8582660a6fd8620e7212ac2ac418
iTom <tianyou.liang@student.uts.edu.au>
2024-11-25 14:16:39 +1100

    update complete_label.sh|py

--- ctpelvic1k/complete_label.sh
+++ ctpelvic1k/complete_label.sh
@@ -1,2 +1,2 @@
 P=$HOME/sd10t/ctpelvic1k
-python ../verse/complete_label.py $P/processed-ctpelvic1k $P/processed-ctpelvic1k-ts-bone-label
+python ../verse/complete_label.py $P/processed-ctpelvic1k $P/processed-ctpelvic1k-ts
--- verse/complete_label.py
+++ verse/complete_label.py
@@ -1,5 +1,5 @@
-import argparse, os, os.path as osp, glob, shutil
-import importlib#, packaging
+import argparse, os, os.path as osp, glob, tempfile
+import importlib.metadata#, packaging
 import numpy as np
 import nibabel as nib
 import totalsegmentator as totalseg
@@ -7,8 +7,7 @@ from totalsegmentator.map_to_binary import class_map
 from totalsegmentator.python_api import totalsegmentator
 
 """
-Complete bone annotation using Total Segmentator.
-Combined labels are binary: {0: non-bone, 1: bone}
+Infer with pretrained TotalSegmentator models to find potential unlabelled bones.
 """
 
 TOTALSEG_VERSION = importlib.metadata.version('totalsegmentator')
@@ -27,22 +26,38 @@ def is_bone(pred_np):
         return ((25 <= pred_np) & (pred_np <= 50)) | ((69 <= pred_np) & (pred_np <= 78)) + ((91 <= pred_np) & (pred_np <= 116))
 
 
-def complete(image_nii, label_nii, save_nii):
-    pred_nib = totalsegmentator(image_nii, './_ts_output')
-    shutil.rmtree('./_ts_output') # rm unused results
-    label_nib = nib.load(label_nii)
-    assert pred_nib.shape == label_nib.shape, f"label: {label_nib.shape}, prediction: {pred_nib.shape}"
-    label = label_nib.get_fdata().astype(np.uint8) # in [0, 4]
-    pred = pred_nib.get_fdata().astype(np.uint8) # in [0, 104]
-    # combine original partial label & TotalSegmentator prediction
-    comb_isb = ((label > 0) | is_bone(pred)).astype(np.uint8)
-    isb_nib = nib.Nifti1Image(comb_isb, affine=label_nib._affine, header=label_nib.header)
-    os.makedirs(osp.dirname(save_nii) or '.', exist_ok=True)
-    nib.save(isb_nib, save_nii)
+# def complete(image_nii, label_nii, save_nii):
+#     pred_nib = totalsegmentator(image_nii, './_ts_output')
+#     shutil.rmtree('./_ts_output') # rm unused results
+#     label_nib = nib.load(label_nii)
+#     assert pred_nib.shape == label_nib.shape, f"label: {label_nib.shape}, prediction: {pred_nib.shape}"
+#     label = label_nib.get_fdata().astype(np.uint8) # in [0, 4]
+#     pred = pred_nib.get_fdata().astype(np.uint8) # in [0, 104]
+#     # combine original partial label & TotalSegmentator prediction
+#     comb_isb = ((label > 0) | is_bone(pred)).astype(np.uint8)
+#     isb_nib = nib.Nifti1Image(comb_isb, affine=label_nib._affine, header=label_nib.header)
+#     os.makedirs(osp.dirname(save_nii) or '.', exist_ok=True)
+#     nib.save(isb_nib, save_nii)
 
-    # print("test shape consistency")
-    # isb_nib = nib.load(save_nii)
-    # assert label.shape == isb_nib.shape, f"label: {label_nib.shape}, re-read: {isb_nib.shape}"
+#     # print("test shape consistency")
+#     # isb_nib = nib.load(save_nii)
+#     # assert label.shape == isb_nib.shape, f"label: {label_nib.shape}, re-read: {isb_nib.shape}"
+
+
+def predict(image_nii_f, vid, save_dir):
+    """predict with pretrained model of several bone-related subtasks, and save"""
+    for subtask in ("total", "appendicular_bones", "vertebrae_body"):
+        save_f = os.path.join(save_dir, f"{vid}-{subtask}.nii.gz")
+        if not os.path.isfile(save_f):
+            with tempfile.TemporaryDirectory() as temp_dir:
+                pred_nib = totalsegmentator(image_nii_f, temp_dir, task=subtask)
+            nib.save(pred_nib, save_f)
+            print(vid, subtask, end='\r')
+
+
+def combine():
+    """combine given label and prediction."""
+    pass
 
 
 if "__main__" == __name__:
@@ -52,10 +67,12 @@ if "__main__" == __name__:
     args = parser.parse_args()
 
     assert osp.isdir(args.src), args.src
+    os.makedirs(args.dest, exist_ok=True)
     for img_f in glob.glob(osp.join(args.src, "*_image.nii.gz")):
         lab_f = img_f[:-len("_image.nii.gz")] + "_label.nii.gz"
         assert osp.isfile(lab_f), f"image: {img_f}, label: {lab_f}"
-        stem = osp.basename(img_f)[:-len("_image.nii.gz")]
-        print(stem, end='\r')
-        save_f = osp.join(args.dest, stem + "_label_ts.nii.gz")
-        complete(img_f, lab_f, save_f)
+        vid = osp.basename(img_f)[:-len("_image.nii.gz")]
+        print(vid, end='\r')
+        # save_f = osp.join(args.dest, stem + "_label_ts.nii.gz")
+        # complete(img_f, lab_f, vid)
+        predict(img_f, vid, args.dest)
